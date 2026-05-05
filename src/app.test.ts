f31c32269892b7967c1cb2a05f30ee11a77b1889
Geoff Speirs <geoff.dotvital@gmail.com>
2026-05-06 00:10:42 +1000

    use typescript

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import { render } from './app.js';
+import { render } from './app.ts';
 
 describe('render', () => {
   it('returns "Hello World"', () => {
